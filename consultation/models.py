0bec64aa876d9073859180a3b14cceed7ea00766
David <saumet.david@orange.fr>
2024-08-30 23:34:19 +0200

    add cons

--- consultation/models.py
+++ consultation/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
 class Media(models.Model):
-    # Vos champs ici
     titre = models.CharField(max_length=100)
     disponible = models.BooleanField(default=True)
-    # Autres champs selon vos besoins
 
     def __str__(self):
         return self.titre
@@ -13,3 +11,7 @@ class Consultation(models.Model):
     emprunteur = models.ForeignKey('bibliothecaire.Emprunteur', on_delete=models.CASCADE)
     media_consulte = models.ForeignKey('bibliothecaire.Media', on_delete=models.CASCADE)
     date_consultation = models.DateField()
+
+    def __str__(self):
+        return f"{self.emprunteur.name} a consulté {self.media_consulte.name}"
+
--- consultation/tests.py
+++ consultation/tests.py
@@ -1,17 +1,27 @@
 from django.test import TestCase
 from .models import Consultation
 from bibliothecaire.models import Media, Emprunteur
-
+from datetime import date
 
 class ConsultationModelTests(TestCase):
-    fixtures = ['second_data.json']  # Charger les données initiales pour le test
+    fixtures = ['second_data.json', 'initial_data.json']
 
     def test_creation_consultation(self):
-        consultation = Consultation.objects.get(pk=1)
+        # Créer les objets nécessaires pour la consultation
+        emprunteur = Emprunteur.objects.get(pk=1)
+        media = Media.objects.get(pk=1)
+
+        # Créer l'instance de Consultation
+        consultation = Consultation.objects.create(
+            emprunteur=emprunteur,
+            media_consulte=media,
+            date_consultation=date(2024, 8, 27)
+        )
 
         # Vérifier que la consultation a bien été créée avec les bonnes données
         self.assertEqual(consultation.emprunteur.name, "John Doe")
         self.assertEqual(consultation.media_consulte.name, "Python Basics")
         self.assertEqual(str(consultation.date_consultation), "2024-08-27")
         self.assertEqual(str(consultation), "John Doe a consulté Python Basics")
+
 
